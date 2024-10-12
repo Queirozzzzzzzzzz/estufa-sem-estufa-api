8ebadddcdc1355eb4413dab60c72cb61298f9cff
Queirozzzzzzzzzz <vinicius.queiroz.coding@gmail.com>
2024-10-12 13:04:14 +0000

    add `authentication requirement` to `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,30 @@
+import nextConnect from "next-connect";
+
+import controller from "models/controller";
+import authentication from "models/authentication";
+import validator from "models/validator";
 import db from "infra/database";
 
-export default async function status(req, res) {
+export default nextConnect({
+  attachParams: true,
+  onNoMatch: controller.onNoMatchHandler,
+  onError: controller.onErrorHandler,
+})
+  .use(controller.injectRequestMetadata)
+  .use(controller.logRequest)
+  .get(getValidationHandler, authentication.authorize, getHandler);
+
+async function getValidationHandler(req, res, next) {
+  const cleanedData = validator(req.cookies, {
+    token: "required",
+  });
+
+  req.cookies = cleanedData;
+
+  next();
+}
+
+async function getHandler(req, res) {
   const updatedAt = new Date().toISOString();
 
   const dbName = process.env.POSTGRES_DB;
